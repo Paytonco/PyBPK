80f831c2675f8c3e21efded31a1460576396a477
Payton Thomas <paytonco@gmail.com>
2021-02-18 12:02:06 -0700

    Corrected license and setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='PyBPK',
       version='0.3',
       description='Python-Based Pharmacokinetics',
-      url='',
+      url='https://github.com/Paytonco/PyBPK',
       author='Payton J Thomas',
       author_email='u1257054@umail.utah.edu',
       license='MIT',
